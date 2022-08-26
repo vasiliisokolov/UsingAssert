f2b4ad7142e813618ae284768d4b239dc408a007
VasiliiSokolov <vasilysokolov@mail.ru>
2022-08-26 18:39:17 +0300

    Добавьте файлы проекта.

---
+++ UsingAssert/UsingAssert.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cassert>
+
+float travelTime(float, float);
+
+int main()
+{
+    float distance, float speed;
+    std::cout << "Enter distance of your Jorney:\n";
+    std::cin >> distance;
+    std::cout << "Enter your speed:\n";
+    std::cin >> speed;
+
+    std::cout << "Time of your Travel: " << travelTime(distance, speed);
+}
+
+
+float travelTime(float distance, float speed)
+{
+    assert(distance >= 0 && speed >= 0);
+    float time = distance / speed;
+    return time;
+}
